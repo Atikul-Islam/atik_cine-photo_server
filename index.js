d6682325b7c888f428bfe7469e2180502e1deee4
Atikul-Islam <atikmina25@gmail.com>
2022-11-20 20:39:50 +0600

    Done all setup & final commit

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//username: dbUser1
-//pass: W2FuRoTftfGtcHo2
 
 //added mongo & services
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zp5ddvi.mongodb.net/?retryWrites=true&w=majority`;
@@ -19,7 +17,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         const servicesCollection = client.db('cinePhoto').collection('services')
-        const reviewsCollection = client.db('lockpc').collection('reviews')
+        const reviewsCollection = client.db('cinePhoto').collection('reviews')
         
 //services
         app.get('/', async (req, res) => {
@@ -59,12 +57,13 @@ async function run(){
             res.send(reviews)
         })
 
-        app.get('review/:id', async (req, res) => {
+        app.get('/reviews/:id', async (req, res) => {
             const id = req.params.id
-            const query = {_id: parseInt(id)}
+            const query = {s_id:id}
             const cursor = reviewsCollection.find(query)
             const result = await cursor.toArray()
             res.send(result)
+            
         })
 
         app.delete('/reviews/:id', async(req, res) => {
@@ -72,16 +71,23 @@ async function run(){
             const query = {_id: ObjectId(id)}
             const result = await reviewsCollection.deleteOne(query)
             res.send(result)
+
         })
 
         app.patch('/reviews/:id', async (req, res) => {
             const id = req.params.id;
-            const message = req.body.message;
+            const comment = req.body.comment;
             const query = {_id: ObjectId(id)}
             const updatedMessage = {
-                $set:{ message: message}
+                $set:{ comment: comment}
             }
             const result = await reviewsCollection.updateOne(query,updatedMessage)
+            res.send(result)
+        })
+
+        app.post('/services', async (req, res) => {
+            const service = req.body
+            const result = await servicesCollection.insertOne(service)
             res.send(result)
         })
     }
